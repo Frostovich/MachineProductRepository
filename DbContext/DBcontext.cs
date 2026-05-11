b2505eb5b730b962f101b2fc20edb3789e2319c8
fast <jknukesloler@gmail.com>
2026-05-11 11:09:38 +0900

    Отправка MVP

---
+++ DbContext/DBcontext.cs
@@ -0,0 +1,55 @@
+﻿namespace Machine_Product_Service.DbContext;
+using Machine_Product_Service.User;
+using Machine_Product_Service.MachineProduct;
+using Microsoft.EntityFrameworkCore;
+public class DBcontext : DbContext
+{
+    DbSet<User>  Users { get; set; }
+    DbSet<Machine> Machines { get; set; }
+
+    public DBcontext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Products)
+            .WithOne(p => p.user)
+            .HasForeignKey(u => u.MachineId)
+            .OnDelete(DeleteBehavior.Cascade);
+        
+        modelBuilder.Entity<Machine>()
+            .Property(m => m.MachineModel)
+            .HasMaxLength(256)
+            .IsRequired();
+
+        modelBuilder.Entity<Machine>()
+            .Property(m => m.MachineName)
+            .HasMaxLength(256)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.UserName)
+            .HasMaxLength(256)
+            .IsRequired();
+        
+        modelBuilder.Entity<User>()
+            .Property(u => u.Password)
+            .HasMaxLength(256)
+            .IsRequired();
+            
+        modelBuilder.Entity<Machine>()
+            .Property(m => m.MachineRun)
+            .HasMaxLength(256)
+            .IsRequired();
+        
+        modelBuilder.Entity<Machine>()
+            .Property(m => m.MachineDescription)
+            .HasMaxLength(256)
+            .IsRequired();
+
+    }
+}
---
+++ MachineProduct/Machine.cs
@@ -0,0 +1,27 @@
+﻿namespace Machine_Product_Service.MachineProduct;
+using Machine_Product_Service.User;
+public class Machine
+{
+    public long MachineGuid
+    { get; set; }
+    
+    public int MachineId 
+    { get; set; }
+    
+    public string MachineName 
+    { get; set; }
+    
+    public string MachineModel 
+    { get; set; }
+    
+    public string MachineDescription 
+    { get; set; }
+    
+    public int  MachineRun 
+    { get; set; }
+    
+    public int MachineYear { get; set; }
+    
+    public User user 
+    { get; set; }
+}
